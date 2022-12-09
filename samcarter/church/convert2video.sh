30b7a9924175312c2858d9c8578ca1fedf66c8a1
samcarter <sam.carter8@aol.com>
2022-12-09 15:58:36 +0100

    let the pandas sing!

---
+++ samcarter/church/convert2video.sh
@@ -0,0 +1,18 @@
+# clean up old video
+rm church.mp4
+
+# convert to png images
+pdftoppm -png -r 240 church.pdf church
+
+# convert to video
+ffmpeg  -ss 00:00:00 -i church-%03d.png -ss 00:00:18 -i BBC_One_-_Songs_of_Praise_The_Nativity_O_Come_All_Ye_Faithful-p08mdj9p.m4a -shortest church_raw.mp4
+
+# repair video
+HandBrakeCLI --crop 0:0:0:0  -i church_raw.mp4 -o church.mp4
+
+# clean up
+rm church-*.png
+rm church_raw.mp4
+
+# view :)
+open church.mp4
